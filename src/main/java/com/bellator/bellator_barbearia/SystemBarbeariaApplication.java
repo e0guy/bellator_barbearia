1c9acdd6dd4cf7464166adc5b770b4ab1eb2d114
José Ramos <joseramosdossantosjunior@gmail.com>
2026-03-15 15:20:17 -0300

    Reestruturação

---
+++ src/main/java/com/bellator/bellator_barbearia/SystemBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.bellator.bellator_barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBarbeariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bellator/bellator_barbearia/SystemBarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellator.bellator_barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
